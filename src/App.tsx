5189cc454aca8ceeb22023538a3fc00a6cb667c2
Joey van der Valk <joeyvandervalk@gmail.com>
2022-11-14 20:23:47 +0100

    created auth and added dark mode toggle. Functionality still to do

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Auth from "./components/Auth";
 import Navbar from "./components/layout/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <main className="min-w-screen min-h-screen">
+      <Navbar />
+      <Auth />
+    </main>
+  );
 }
 
 export default App;
--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const Auth: React.FC = () => {
-  return <div>Auth</div>;
+  return (
+    <section className=" mx-auto my-20 flex max-w-sm flex-col items-center justify-center gap-5">
+      <article className="px-2">
+        <h1 className="mb-3 text-5xl">Sign in</h1>
+        <p>
+          To be able to access the chatroom, you will need to log in. Please
+          enter your email below. You will receive a magic link so you can log
+          in without the use of a password.
+        </p>
+      </article>
+      <div className="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
+        <div className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="email"
+              className="input-bordered input"
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn-primary btn">Login</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Auth;
