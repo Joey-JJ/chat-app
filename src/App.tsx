de6672003af1b9cb9027eac842feceda4fbfc555
Joey van der Valk <joeyvandervalk@gmail.com>
2022-11-14 21:53:24 +0100

    Fixed auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { supabase } from "./utils/supabaseClient";
+import { sessionContext } from "./utils/sessionContext";
 import Auth from "./components/Auth";
 import Navbar from "./components/layout/Navbar";
 
-function App() {
+const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    let mounted = true;
+
+    const fetchSession = async () => {
+      const {
+        data: { session: any },
+      } = await supabase.auth.getSession();
+
+      if (mounted) {
+        if (session) {
+          setSession(session);
+        }
+
+        setIsLoading(false);
+      }
+    };
+
+    fetchSession();
+
+    const { subscription }: any = supabase.auth.onAuthStateChange(
+      (_event, session: any) => {
+        setSession(session);
+      }
+    );
+
+    return () => {
+      mounted = false;
+      subscription?.unsubscribe();
+    };
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+
   return (
-    <main className="min-w-screen min-h-screen">
+    <sessionContext.Provider value={{ session, setSession }}>
       <Navbar />
-      <Auth />
-    </main>
+      <div className="App">{session ? <></> : <Auth />}</div>
+    </sessionContext.Provider>
   );
-}
+};
 
 export default App;
--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { supabase } from "../utils/supabaseClient";
 
 const Auth: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const { error } = await supabase.auth.signInWithOtp({ email });
+      if (error) throw error;
+      alert("Check your email for the login link!");
+    } catch (error: any) {
+      alert(error.error_description || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <section className=" mx-auto my-20 flex max-w-sm flex-col items-center justify-center gap-5">
       <article className="px-2">
@@ -12,21 +31,24 @@ const Auth: React.FC = () => {
         </p>
       </article>
       <div className="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
-        <div className="card-body">
+        <form className="card-body" onSubmit={handleLogin}>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
             <input
-              type="text"
+              type="email"
               placeholder="email"
               className="input-bordered input"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="form-control mt-6">
-            <button className="btn-primary btn">Login</button>
+            <button className={`btn-primary btn ${loading ? "disabled" : ""}`}>
+              {!loading ? "Login" : "Loading..."}
+            </button>
           </div>
-        </div>
+        </form>
       </div>
     </section>
   );
