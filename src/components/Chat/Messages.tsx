8ae1bccb4d6c56e45b7309c6860b9459f620cb35
Joey van der Valk <joeyvandervalk@gmail.com>
2022-11-15 18:40:51 +0100

    Added fetching of messages + loading state

--- src/components/Chat/Messages.tsx
+++ src/components/Chat/Messages.tsx
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { supabase } from "../../utils/supabaseClient";
 import ChatBubble from "./ChatBubble";
 
 export const Messages: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [messages, setMessages] = useState<any[]>([]);
+
+  useEffect(() => {
+    setLoading(true);
+    supabase
+      .from("messages")
+      .select("*")
+      .then((res) => {
+        setMessages(res.data as any);
+      });
+    setLoading(false);
+  }, []);
+
   return (
-    <div className="flex max-h-[calc(100vh-128px)] flex-col overflow-scroll bg-base-300">
-      <ChatBubble text={"Hello, this is a message."} own={false} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
-      <ChatBubble text={"Hello, this is another message."} own={true} />
+    <div className="flex h-[calc(100vh-128px)] flex-col overflow-scroll bg-base-300">
+      {loading && <div className="mt-4 text-center">Loading...</div>}
+      {!loading && messages.length === 0 && (
+        <div className="mt-4 text-center">No messages yet</div>
+      )}
+      {messages.length > 0 &&
+        !loading &&
+        messages.map((message) => (
+          <ChatBubble key={message.id} text={message.message} own={false} />
+        ))}
     </div>
   );
 };
