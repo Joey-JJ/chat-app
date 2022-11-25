0bd00ef9a54fcee2f5339c2c396753ec9dd5b345
Joey <joeyvandervalk@gmail.com>
2022-11-25 12:30:41 +0100

    Removed comment and added margin to chatbubble

--- src/App.tsx
+++ src/App.tsx
@@ -117,31 +117,6 @@ const App: React.FC = () => {
         submitHandler={submitHandler}
         setUsername={setUsername}
       />
-      {/* <div className="modal">
-        <div className="modal-box max-w-xs p-10">
-          <h3 className="font-bold text-lg">You need a username to chat!</h3>
-          <p className="py-4">
-            Please enter a username (max 8 characters) below to start chatting.
-          </p>
-          <form
-            onSubmit={submitHandler}
-            className="modal-action place-content-center flex flex-col mx-auto gap-2"
-          >
-            <input
-              type="text"
-              placeholder="Username"
-              className="input input-primary input-md"
-              value={username}
-              onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                setUsername(e.currentTarget.value)
-              }
-            />
-            <button className="btn btn-primary btn-md" type="submit">
-              Start chatting!
-            </button>
-          </form>
-        </div>
-      </div> */}
     </div>
   );
 };
--- src/components/chat/ChatBubble.tsx
+++ src/components/chat/ChatBubble.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const ChatBubble: React.FC<Props> = ({ username, text, own }) => {
   return (
-    <div className={`flex flex-col ${own ? "self-end" : ""}`}>
+    <div className={`flex flex-col my-2 ${own ? "self-end" : ""}`}>
       <div
         className={`mx-4 mt-3 w-max max-w-[18rem] sm:max-w-lg rounded-lg bg-primary${
           own ? "-focus" : ""
